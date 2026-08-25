6a88aa4726ffe50b71f3311c15254436d86a2026
Rémy Ruiz <rruiz@student.42lehavre.fr>
2026-08-25 10:25:47 +0200

    201, 202, 203 realisé

--- src/gui/main_window.go
+++ src/gui/main_window.go
@@ -19,23 +19,50 @@ import (
 	"fyne.io/fyne/v2/app"
 )
 
-func CreateMainWindow(a fyne.App) (fyne.Window, error) {
+func CreateMainWindow(a fyne.App, fullscreen bool) (fyne.Window, error) {
+	// Create the Window with his title
 	w := a.NewWindow("The Gambling Protocol")
-	w.SetFullScreen(true)
+
+	// Whether to open the window in full-screen mode or not, used only during testing
+	if fullscreen {
+		w.SetFullScreen(true)
+	} else {
+		w.Resize(fyne.NewSize(1024, 768))
+		w.CenterOnScreen()
+	}
+
+	// Load icon.png if error occurs, return an error
 	res, err := fyne.LoadResourceFromPath("assets/other/icon.png")
+
+	// Returns nil and the error, or the window without an error
 	if err != nil {
 		return nil, err
 	}
+	// Set the icon
 	w.SetIcon(res)
 	return w, nil
 }
 
-func Run() {
-	a := app.NewWithID("com.theanswerprotocol.app")
-	window, err := CreateMainWindow(a)
+func Run(fullscreen bool) {
+	// Create a new app instance use for all window
+	a := app.NewWithID("theanswerprotocol")
+
+	// Create the window, if an error occurs exit with a clear error message
+	window, err := CreateMainWindow(a, fullscreen)
 	if err != nil {
 		utils.ExitError("Error during the window creation", err)
 	}
-	window.SetContent(HomeView())
+
+	var size fyne.Size
+	// Set the window size because Go has trouble with fullscreen mode
+	if fullscreen {
+		size = fyne.NewSize(1920, 1080)
+	} else {
+		size = window.Canvas().Size()
+	}
+
+	// Displays the window's default content on the home screen
+	window.SetContent(HomeView(size))
+
 	window.ShowAndRun()
 }
