6a75b9b27009af9f20290c88177a3d515bfe5af3
Rémy Ruiz <rruiz@student.42lehavre.fr>
2026-09-07 16:49:39 +0200

    allCommand normalement bien formé

--- src/gui/game/command_widget.go
+++ src/gui/game/command_widget.go
@@ -6,14 +6,13 @@
 /* By: emarette, rruiz, alebaron                 +#+  +:+       +#+        */
 /*                                             +#+#+#+#+#+   +#+           */
 /* Created: 2026/09/01 09:04:16 by rruiz           #+#    #+#              */
-/* Updated: 2026/09/01 14:58:41 by rruiz           ###   ########.fr       */
+/* Updated: 2026/09/07 16:26:05 by rruiz           ###   ########.fr       */
 /*                                                                         */
 /* *********************************************************************** */
 
 package game
 
 import (
-	"fmt"
 	"image/color"
 	"io"
 
@@ -22,6 +21,47 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+var list = "list"
+var field = "field"
+
+type parameter struct {
+	name        string
+	form        string
+	next        *parameter
+	placeholder string
+}
+
+type commandModel struct {
+	name       string
+	parameters parameter
+}
+
+var allCommand = map[string]commandModel{
+	"LOOK": {name: "LOOK"},
+	"MOVE": {name: "MOVE", parameters: parameter{name: "direction", form: list, placeholder: "Select a direction."}},
+	"WHO":  {name: "WHO"},
+
+	"TALK":  {name: "TALK", parameters: parameter{name: "npc", form: list, placeholder: "Choose an NPC to talk to."}},
+	"CHAT":  {name: "CHAT", parameters: parameter{name: "scope", form: list, placeholder: "Choose the scope of the message.", next: &parameter{name: "text", form: field, placeholder: "Enter your message"}}},
+	"GROUP": {name: "GROUP", parameters: parameter{name: "action", form: list, placeholder: "Choose a group action."}},
+
+	// "ATTACK":    {name: "ATTACK"},
+	"STATUS": {name: "STATUS"},
+
+	// "QUEST":     {name: "QUEST"},
+	// "QUESTS":    {name: "QUESTS"},
+
+	"INVENTORY": {name: "INVENTORY"},
+	"USE":       {name: "USE", parameters: parameter{name: "item", form: list, placeholder: "Choose an object to use."}},
+	"TRADE":     {name: "TRADE", parameters: parameter{name: "trade things", form: list, placeholder: "Choose a merchant."}},
+	"BUY":       {name: "BUY", parameters: parameter{name: "trader", form: list, placeholder: "Choose a merchant.", next: &parameter{name: "item", form: list, placeholder: "Choose an item to buy.", next: &parameter{name: "quantity", form: field, placeholder: "Enter the quantity."}}}},
+	"SELL":      {name: "SELL", parameters: parameter{name: "trader", form: list, placeholder: "Choose a merchant.", next: &parameter{name: "item", form: list, placeholder: "Choose an item to sell.", next: &parameter{name: "quantity", form: field, placeholder: "Enter the quantity."}}}},
+	"TAKE":      {name: "TAKE", parameters: parameter{name: "take item", form: list, placeholder: "Choose an item to pick-up"}},
+	"DROP":      {name: "DROP", parameters: parameter{name: "drop item", form: list, placeholder: "Choose an item to drop"}},
+
+	"GAMBLING": {name: "GAMBLING", parameters: parameter{name: "quantity", form: "field", placeholder: "Enter the amount (in gambling coins)."}},
+}
 
 var commandByCategory = map[string][]string{
 	"Environment": {"LOOK", "MOVE", "WHO"},
@@ -71,5 +111,5 @@ func createButton(category string, subCommandsBox *fyne.Container, stdin io.Writ
 }
 
 func executeCommand(stdin io.WriteCloser, command string) {
-	fmt.Fprintf(stdin, "%s\n", command)
+	commandInfo := allCommand[command]
 }
