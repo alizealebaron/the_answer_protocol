d124db0baf5661f0dd18e2cea39b8a542ce8e40b
Rémy Ruiz <rruiz@student.42lehavre.fr>
2026-09-07 13:40:32 +0200

    215 fait

---
+++ src/gui/game/players_widget.go
@@ -0,0 +1,48 @@
+/* *********************************************************************** */
+/*                                                                         */
+/*                                                     :::      ::::::::   */
+/* players_widget.go                                 :+:      :+:    :+:   */
+/*                                                 +:+ +:+         +:+     */
+/* By: emarette, rruiz, alebaron                 +#+  +:+       +#+        */
+/*                                             +#+#+#+#+#+   +#+           */
+/* Created: 2026/09/07 11:17:14 by rruiz           #+#    #+#              */
+/* Updated: 2026/09/07 13:32:44 by rruiz           ###   ########.fr       */
+/*                                                                         */
+/* *********************************************************************** */
+
+package game
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type WhoInfo struct {
+	Room   []string `json:"room"`
+	Server int      `json:"server"`
+}
+
+func playerCountLabel(listener *Listener) *fyne.Container {
+	label := widget.NewLabel("Use the “WHO” command to view information about players number.")
+	listener.Subscribe(func(line string) {
+		if strings.HasPrefix(line, "OK { \"room\":") {
+			whoJson := strings.TrimPrefix(line, "OK ")
+			var data WhoInfo
+			err := json.Unmarshal([]byte(whoJson), &data)
+			if err != nil {
+				return
+			}
+			label.SetText(fmt.Sprintf("Nombre de joueur dans la room: %d\nNombre de joueur global: %d", len(data.Room), data.Server))
+			fmt.Println(line)
+		}
+	})
+	label.Wrapping = fyne.TextWrapWord
+	label.Alignment = fyne.TextAlignCenter
+
+	return container.NewCenter(label)
+}
